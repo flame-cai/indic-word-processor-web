7b0e3d507e2ceda15f8ca855db61a23ba2307a98
Bharath314 <valabojubharath@yahoo.com>
2024-11-05 16:39:00 +0530

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script setup>
+import Editor from 'primevue/editor'
+</script>
+
+<template>
+  <header>
+    <nav><h1>Indic Word Processor</h1></nav>
+  </header>
+  <Editor>
+    <template v-slot:toolbar>
+      <span class="ql-formats">
+        <button v-tooltip.bottom="'Bold'" class="ql-bold"></button>
+        <button v-tooltip.bottom="'Italic'" class="ql-italic"></button>
+        <button v-tooltip.bottom="'Underline'" class="ql-underline"></button>
+      </span>
+    </template>
+  </Editor>
+</template>
+
+<style>
+header {
+  margin-bottom: 1em;
+}
+
+.p-editor {
+  height: 90%;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,28 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import PrimeVue from 'primevue/config'
+import Aura from '@primevue/themes/aura'
+import Tooltip from 'primevue/tooltip'
+
+import App from './App.vue'
+import router from './router'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura,
+    options: {
+      darkModeSelector: 'system',
+    },
+  },
+  ripple: true,
+})
+app.directive('tooltip', Tooltip)
+
+app.mount('#app')
